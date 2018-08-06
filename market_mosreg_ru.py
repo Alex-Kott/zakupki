8ececc1bedc5152263ba887c217cdb40de9fed7e
Alexey Kott <alexey.kott@gmail.com>
2018-08-06 08:50:44 +0300

    zzzzzz

--- market_mosreg_ru.py
+++ market_mosreg_ru.py
@@ -1,12 +1,32 @@
 import asyncio
+import json
+from typing import Dict
 
 from aiohttp import ClientSession
+from bs4 import BeautifulSoup
+
+
+async def parse_procurement(session: ClientSession, item: Dict[str, str]):
+    headers = {
+        "XXX-TenantId-Header": "2"
+    }
+    async with session.get(f"https://api.market.mosreg.ru/api/Trade/{item['Id']}/GetTradeDocuments", headers=headers) as response:
+        raw_response = await response.text()
+        data = json.loads(raw_response)
+        file_links = [item["Url"] for item in data]
+
+    async with session.get(f"https://market.mosreg.ru/Trade/ViewTrade", params={'id': item['Id']}) as response:
+        raw_response = await  response.text()
+        soup = BeautifulSoup(raw_response, "lxml")
+        
+
+
 
 
 async def main():
     get_trades_url = "https://api.market.mosreg.ru/api/Trade/GetTradesForParticipantOrAnonymous"
     payload = {"page": 1,
-               "itemsPerPage": 100,
+               "itemsPerPage": 1,
                "tradeState": "15",
                "OnlyTradesWithMyApplications": False,
                "sortingParams": [],
@@ -39,13 +59,27 @@ async def main():
     async with ClientSession() as session:
         async with session.post(get_trades_url, headers=headers, data=payload) as response:
             raw_response = await response.text()
-            print(raw_response)
+            # print(raw_response)
             # for k, v in response.__dict__.items():
             #     print(k)
             #     print(v, end='\n\n')
 
-            with open('mosreg_response.json', 'w') as file:
-                file.write(raw_response)
+            # with open('mosreg_response.json', 'w') as file:
+            #     file.write(raw_response)
+
+            data = json.loads(raw_response)
+
+            procurement_list = data['invdata']
+
+            procurements = [await parse_procurement(session, item) for item in procurement_list]
+
+
+
+
+
+
+
+
 
 
 if __name__ == "__main__":
